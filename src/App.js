a498921b3fd83bc88b3ec62bca249d320ec9697e
Tahmid <tahmid4404@gmail.com>
2023-09-06 11:16:12 -0400

    Save changes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import VideoRecorder from "../src/videorecorder";
+import AudioRecorder from "../src/audiorecorder";
+
+const App = () => {
+    let [recordOption, setRecordOption] = useState("video");
+    const toggleRecordOption = (type) => {
+        return () => {
+            setRecordOption(type);
+        };
+    };
+    return (
+        <div>
+            <h1>React Media Recorder</h1>
+            <div className="button-flex">
+                <button onClick={toggleRecordOption("video")}>
+                  Record Video
+                </button>
+                <button onClick={toggleRecordOption("audio")}>
+                  Record Audio
+                </button>
+            </div>
+            <div>
+                {recordOption === "video" ? <VideoRecorder /> : <AudioRecorder />}
+            </div>
+        </div>
+    );
+};
+export default App;
